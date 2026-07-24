2e6f6d1a41a0e4f86a9e780af6f5338a827db6bc
satria-mahatir <satrianandatama@gmail.com>
2026-07-24 09:12:48 +0700

    fix: remove perpus_app from repository and ignore it

--- perpus_app/android/app/src/main/kotlin/com/perpustakaan/perpus_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.perpustakaan.perpus_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- perpus_app/lib/core/constants/api_constants.dart
+++
@@ -1,9 +0,0 @@
-class ApiConstants {
-  static String baseUrl = 'http://192.168.110.30:8000/api/v1';
-  
-  static const String kategoris = '/kategoris';
-  static const String penulis = '/penulis';
-  static const String penerbits = '/penerbits';
-  static const String bukus = '/bukus';
-  static const String peminjaman = '/peminjaman';
-}
